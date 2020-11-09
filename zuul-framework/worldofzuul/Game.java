b335b5714de782fc66127f72df8c5b93eaaf74ca
Dilara <dcele20@student.sdu.dk>
2020-11-09 20:58:49 +0100

    More updates

--- zuul-framework/worldofzuul/Game.java
+++ zuul-framework/worldofzuul/Game.java
@@ -93,7 +93,7 @@ public class Game {
     private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Wool!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("World of Wool is a new, incredibly fun learning game.");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
