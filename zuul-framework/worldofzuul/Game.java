05bda5d7d52fa0c32db90d3b6952f254c1d9ba95
Gorm <gokri20@student.sdu.dk>
2020-11-24 10:33:19 +0100

    Deleted command-line interface

--- zuul-framework/worldofzuul/Game.java
+++ zuul-framework/worldofzuul/Game.java
@@ -3,7 +3,6 @@ package worldofzuul;
 import java.util.Scanner;
 
 public class Game {
-    private Parser parser;
     private Room currentRoom;
     private Inventory playerInventory;
     private Materials hemp, linen, bamboo, cotton, polyester;
@@ -14,7 +13,6 @@ public class Game {
 
     public Game() {
         createRooms();
-        parser = new Parser();
         playerInventory = new Inventory();
 
     }
@@ -89,55 +87,7 @@ public class Game {
         currentRoom = mainRoom;
     }
 
-    public void play() {
-        printWelcome();
-
-
-        boolean finished = false;
-        while (!finished) {
-            Command command = parser.getCommand();
-            finished = processCommand(command);
-        }
-        System.out.println("Thank you for playing. Good bye.");
-    }
-
-    private void printWelcome() {
-        System.out.println();
-        System.out.println("Welcome to the World of Wool!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
-        System.out.println();
-        System.out.println(currentRoom.getLongDescription());
-    }
-
-    private boolean processCommand(Command command) {
-        boolean wantToQuit = false;
-
-        CommandWord commandWord = command.getCommandWord();
-
-        if (commandWord == CommandWord.UNKNOWN) {
-            System.out.println("I don't know what you mean...");
-            return false;
-        }
-
-        if (commandWord == CommandWord.HELP) {
-            printHelp();
-        } else if (commandWord == CommandWord.GO) {
-            goRoom(command);
-        } else if (commandWord == CommandWord.GET) {
-            getItem(command);
-        } else if (commandWord == CommandWord.DROP) {
-            dropItem(command);
-        } else if (commandWord == CommandWord.USE) {
-            useItem(command);
-        } else if (commandWord == CommandWord.INVENTORY) {
-            showInventory(command);
-        } else if (commandWord == CommandWord.QUIT) {
-            wantToQuit = quit(command);
-        }
-        return wantToQuit;
-    }
-
+    /*
     // Use materials in farm
     private void useItem(Command command) {
         // Check if there is a second word
@@ -291,19 +241,9 @@ public class Game {
             }
         }
     }
+     */
 
-    private void showInventory(Command command) {
-        if (command.hasSecondWord()) {
-            System.out.println("What?");
-            return;
-        }
-        if (playerInventory.getArrayList().size() == 0) {
-            System.out.println("Your inventory is empty");
-        } else {
-            System.out.println("Your inventory: " + playerInventory.getArrayList());
-        }
-    }
-
+    /*
     // Pick up item method
     private void getItem(Command command) {
         if (!command.hasSecondWord()) {
@@ -373,7 +313,8 @@ public class Game {
         playerInventory.addToInventory(getThisItem);
         System.out.println("You pick up: " + getThisItem);
     }
-
+     */
+    /*
     // Drop item method
     private void dropItem(Command command) {
         if (!command.hasSecondWord()) {
@@ -400,39 +341,6 @@ public class Game {
             }
         }
     }
+     */
 
-    private void printHelp() {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
-        System.out.println();
-        System.out.println("Your command words are:");
-        parser.showCommands();
-    }
-
-    private void goRoom(Command command) {
-        if (!command.hasSecondWord()) {
-            System.out.println("Go where?");
-            return;
-        }
-
-        String direction = command.getSecondWord();
-
-        Room nextRoom = currentRoom.getExit(direction);
-
-        if (nextRoom == null) {
-            System.out.println("There is no door!");
-        } else {
-            currentRoom = nextRoom;
-            System.out.println(currentRoom.getLongDescription());
-        }
-    }
-
-    private boolean quit(Command command) {
-        if (command.hasSecondWord()) {
-            System.out.println("Quit what?");
-            return false;
-        } else {
-            return true;
-        }
-    }
 }
--- zuul-framework/worldofzuul/Main.java
+++ zuul-framework/worldofzuul/Main.java
@@ -3,6 +3,5 @@ package worldofzuul;
 public class Main {
     public static void main(String[] args) {
         Game p1 = new Game();
-        p1.play();
     }
 }
