3470077f00f35d13d40d837657b967b64ae4f2ba
Gorm <gokri20@student.sdu.dk>
2020-11-24 12:44:07 +0100

    Scenebuild

--- zuul-framework/worldofzuul/Main.java
+++ zuul-framework/worldofzuul/Main.java
@@ -1,7 +1,22 @@
 package worldofzuul;
 
-public class Main {
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
         Game p1 = new Game();
+        launch(args);
     }
 }
