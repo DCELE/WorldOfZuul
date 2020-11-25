47bb969b0fc37779e7bd55fdb2a035789bdd903b
Nithurshan Mariyananthajesuthasan <nmari19@student.sdu.dk>
2020-11-25 13:38:05 +0100

    imoort
    cd

--- zuul-framework/worldofzuul/Controller.java
+++ zuul-framework/worldofzuul/Controller.java
@@ -1,5 +1,14 @@
 package worldofzuul;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
